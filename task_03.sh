1c3787662547239165099af90d3c55ad6d647dcf
Ubuntu <ubuntu@ip-172-31-58-77.ec2.internal>
2022-06-23 14:06:32 +0000

    task 4 not done

---
+++ task_03.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#Installing aws-cli based on Linux Hardware Arch
+
+ROOT_UID=0     
+E_NOTROOT=100
+ARCH=$(uname -i)
+
+if [ "$UID" -ne "$ROOT_UID" ]
+then
+    echo "Must have root privilege to run this script."
+    exit $E_NOTROOT
+fi  
+
+which aws &> /dev/null || {
+	echo "Installing aws-cli"
+    which curl &> /dev/null || apt install curl -y && echo "curl is installed"
+    which unzip &> /dev/null || apt install unzip -y && echo "unzip is installed"
+
+    curl "https://awscli.amazonaws.com/awscli-exe-linux-$ARCH.zip" -o "awscliv2.zip"
+    
+    unzip $PWD/awscliv2.zip
+    sudo $PWD/aws/install
+
+	} && {
+       
+		echo "aws-cli 2.7.9 is already installed in your machine"
+	}

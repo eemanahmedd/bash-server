7e6fdd8917e62d2df3c49214f946bae70e861e6f
Ubuntu <ubuntu@ip-172-31-58-77.ec2.internal>
2022-06-26 18:56:41 +0000

    done

--- task_06.sh
+++ task_06.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-rm /var/www/html/*
-mv index.html /var/www/html
+#rm /var/www/html/*
+#mv $PWD/index.html /var/www/html
 
-nginxstatus=$(systemctl status nginx | grep Active | awk '{print $2}')
-  echo $nginxstatus
-sed -i "s/NGINXSTATUS/$nginxstatus/g" /var/www/html/index.html
+nstatus=$(systemctl status nginx | grep Active | awk '{print $2}')
+nversion=$(nginx -v 2>&1 | awk -F' ' '{print $3}' | cut -d / -f 2)
+aversion=$(aws --version | awk '{print $1}' | tr '/' '-')
+sgnum=$(aws ec2 describe-security-groups --query "SecurityGroups[*].{Name:GroupName}"  --output text | wc -l)
+ecnum=$(aws ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId]' --region us-east-1 --output text | wc -l)
+sed -i "s/NGINXSTATUS/$nstatus/g" /var/www/html/index.html
+sed -i "s/NGINXVERSION/$nversion/g" /var/www/html/index.html
+sed -i "s/VERSIONAWS/$aversion/g" /var/www/html/index.html
+sed -i "s/EC2COUNT/$ecnum/g" /var/www/html/index.html
+sed -i "s/SECGRPCOUNT/$sgnum/g" /var/www/html/index.html
